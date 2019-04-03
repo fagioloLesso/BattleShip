5ed5d8aac92a61438e7e41a0b2078b234aafaedb
Simone Cimolato <Simonecimolato@users.noreply.github.com>
2019-04-03 12:47:27 +0200

    Add files via upload
    main

---
+++ battaglia navale.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define DIM 10
+
+generateCPShips();
+
+typedef struct s_nave{
+    int lunghezza;
+    bool orientamento;
+    int posX;
+    int posY;
+}nave;
+
+nave computerBoard[DIM][DIM];
+nave playerBoard[DIM][DIM];
+
+int main(){
+
+
+
+
+return 0;
+}
+
+
+void generateCPShips(){
+    int i;
+    for(i = 0; i <){
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
